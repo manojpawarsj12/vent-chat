89ff0fc0fdda85f6d80b321406cad10054845002
manojpawarsj11 <manojpawarsj.mp11@gmail.com>
2020-09-13 11:17:26 +0530

    some fixes in chat.ejs

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,6 +79,7 @@ module.exports.login_post = async (req, res) => {
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(200).json({ user: user._id });
+    
   } 
   catch (err) {
     const errors = handleErrors(err);
