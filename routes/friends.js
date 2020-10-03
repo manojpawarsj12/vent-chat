942185de1aa6961eee24fe7a7888b7461a3191bb
manojpawarsj11 <manojpawarsj.mp11@gmail.com>
2020-10-03 12:35:37 +0530

    fix recent message bug

--- routes/friends.js
+++ routes/friends.js
@@ -19,18 +19,15 @@ router.get("/friends", requireAuth, async (req, res) => {
       .exec();
     //console.log(kek)
     kek = kek[kek.length - 1];
-    console.log(kek);
+    
     let kek2 = await Conversation.find({ from: friend, to: fr_username._id })
       .sort({ created_at: -1 })
       .exec();
 
     kek2 = kek2[kek2.length - 1];
-    console.log(kek2);
-    if (Date.parse(kek.created_at) > Date.parse(kek2.created_at)) {
-      ongay.push([fr_name, kek.message]);
-    } else {
-      ongay.push([fr_name, kek2.message]);
-    }
+    console.log(kek);
+    ongay.push([friend , fr_name, kek.message,kek.createdAt]);
+    ongay.push([friend , fr_name, kek2.message,kek2.createdAt]);
   }
   console.log(ongay);
   res.json(ongay);
